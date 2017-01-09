c9dba290c41e8490b2f3baddeeda76315564d897
PRADEEP RAJA  MOHAN <mpradeep1994@gmail.com>
2017-01-09 12:57:11 -0600

    Update collect.py

--- collect.py
+++ collect.py
@@ -1,29 +1,36 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[16]:
 
 #import all required packages
-import pickle
-from pathlib import Path
 import sys
 import time
+import csv
+import unicodecsv as csv
 from TwitterAPI import TwitterAPI
-import networkx as nx
-import matplotlib.pyplot as plt
-from collections import Counter, defaultdict, deque
-import copy
-import math
+import pickle
+import numpy as np
+from pathlib import Path
+import requests
 
 
-# In[2]:
+# In[17]:
 
 #twitter API keys for collecting data
+consumer_key = 'tb9zMiyUJ9UeX6blluI4Wlws8'
+consumer_secret = 'XlGNdXHj6Rd2LJ2t9LodnCLydvO2kZ4KLoq95VnQRXfuKXDgZn'
+access_token = '3231404389-56xTceJ255ar5Sj2sQtWuDj8z0grnNedlqOt5AC'
+access_token_secret = 'qwPAoWezVnfgeQCCaJy03rOQUJG2Z9ZACzVPIjcqu7IKD'
+
+
+# In[38]:
+
+#connecting twitter using  API keys for collecting data
 def get_twitter():
     """ Construct an instance of TwitterAPI using the tokens you entered above.
     Returns:
-      An instance of TwitterAPI.
-      
+      An instance of TwitterAPI.     
     """
     consumer_key = 'tb9zMiyUJ9UeX6blluI4Wlws8'
     consumer_secret = 'XlGNdXHj6Rd2LJ2t9LodnCLydvO2kZ4KLoq95VnQRXfuKXDgZn'
@@ -32,11 +39,100 @@ def get_twitter():
     return TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
 
 
-# In[3]:
+# In[62]:
 
-#open the pickle and get the content of the file
-def open_file(tweets,fname):
-    fname=fname+'.pkl'
+#roburst request block collecting data from Twitter rest API
+def get_tweets(twitter,hashtags,tweets,since_id):
+    while True:
+        try:
+            #parameters for request method
+            request = twitter.request('search/tweets',{'q':hashtags,'count':100,'lang':'en','max_id':since_id})
+            for response in request:
+                tweets.append(response)
+            print (len(tweets))
+            return tweets
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            break     
+
+
+# In[54]:
+
+#get friends based on the screen name of a user
+def get_friends(screen_name): 
+    return sorted([friend_id for friend_id in twitter.request('friends/ids',{'screen_name' : screen_name, 'count' :5000})])
+
+
+# In[55]:
+
+#select the fields required for future processing the Json response of the Twitter
+
+def select_required(tweet,M,F):
+    data = []
+    for each_tweet in tweet:
+        name = each_tweet['user']['name']
+        if name:
+            tweet_data = {}
+            tweet_data['screen_name']=each_tweet['user']['screen_name']
+            tweet_data['userid']= each_tweet['user']['id']
+            tweet_data['description']=each_tweet['user']['description']
+            tweet_data['tweet']=each_tweet['text']
+            tweet_data['username']=each_tweet['user']['name']
+            tweet_data['since_id']=each_tweet['id']
+            tweet_data['location']= each_tweet['user']['location']
+            tweet_data['gender']=gender_by_name(name, M, F)
+            if 'retweeted_status' in each_tweet.keys():
+                tweet_data['fcount']=each_tweet['retweeted_status']['favorite_count']
+            else:
+                tweet_data['fcount']= 1
+            data.append(tweet_data)
+    return data
+
+
+# In[56]:
+
+#get name list from the census data and start identifying use name to be a male or female
+def names():
+    males_url = 'http://www2.census.gov/topics/genealogy/' +                 '1990surnames/dist.male.first'
+    females_url = 'http://www2.census.gov/topics/genealogy/' +                   '1990surnames/dist.female.first'
+    males = requests.get(males_url).text.split('\n')
+    females = requests.get(females_url).text.split('\n')
+    male_names = set([m.split()[0].lower() for m in males if m])
+    female_names = set([f.split()[0].lower() for f in females if f])
+
+
+    # Keep names that are more frequent in one gender than the other.
+    males_pct = dict([(m.split()[0].lower(), float(m.split()[1]))
+                      for m in males if m])
+    females_pct = dict([(f.split()[0].lower(), float(f.split()[1]))
+                        for f in females if f])
+
+    male_names = set([m for m in male_names if m not in female_names or
+                  males_pct[m] > females_pct[m]])
+    female_names = set([f for f in female_names if f not in male_names or
+                  females_pct[f] > males_pct[f]])
+    return male_names,female_names
+
+
+# In[57]:
+
+#gender classification
+def gender_by_name(name, male_names, female_names):
+    if name:
+        first = name.split()[0].lower()
+        if first in male_names:
+            return ('male')
+        elif first in female_names:
+            return ('female')
+        else:
+            return ('male')
+
+
+# In[58]:
+
+#open the pickle and get the content of the file for appending new data
+def open_file(tweets,filename):
+    fname= filename+'.pkl'
     p = Path(fname)
     if p.is_file():
         with open(fname, "rb") as file:
@@ -47,202 +143,60 @@ def open_file(tweets,fname):
     return tweets
 
 
-# In[4]:
+# In[59]:
 
-#roburst request block collecting data from Twitter rest API
-def robust_request(twitter, screen_name, max_tries=1):
-     for i in range(max_tries):
-        request = twitter.request('friends/ids',{'screen_name' : screen_name, 'count' :5000})
-        if request.status_code == 200:
-            return request
-        else:
-            print('Got error %s \nsleeping for 15 minutes.' % request.text)
-            sys.stderr.flush()
-            time.sleep(61 * 15)
-
-
-# In[5]:
-
-#find the unique users by avoiding repetition 
-def find_unique_user(tweets):
-    user=[]
-    user_id=[]
-    for i in tweets:
-        user.append(i['screen_name'])
-        user_id.append(i['userid'])
-    return list(set(user)),list(set(user_id))
-
-
-# In[6]:
-
-#finding the friends of the each user
-def find_twitter_frnds(tweeter_names,frnds_id):
-    for each in tweeter_names[:5]:
-        print (each)
-        frnds_id[each] = sorted([friend_id for friend_id in robust_request(twitter,each)])
-    return frnds_id
-
-
-# In[7]:
-
-#build graph by drawing edges for users who follow mutually two teams 
-def draw_graph(names):
-    
-    """
-    Create the  graph from ids of the followers. 
-    """
-    
-    teams = list(names.keys())
-    g = nx.Graph()
-    for i in range(len(teams)):
-        for j in range(i+1,len(teams)):
-            a =list(set(names[teams[i]] ) & set(names[teams[j]]))
-            for node in a:
-                g.add_edges_from([(teams[i],node)]) 
-                g.add_edges_from([(teams[j],node)])
-                #g.add_edges_from([(teams[i],teams[j])])
-
-    return g
-
-
-# In[8]:
-
-get_ipython().magic('matplotlib inline')
-"""draw edges for the users and the team"""
-def read_graph(g,names):
-    
-    labels = {}
-    pos = nx.spring_layout(g)
-    plt.figure(figsize=(100,100))
-    nx.draw(g, pos, with_labels=False,node_size =2500)
-    for node in g.nodes():
-        for i in names:
-            if i == node:
-                labels[node] = i
-   
-    nx.draw_networkx_labels(g,pos,labels=labels,font_size=50,font_color='b')
-    plt.savefig("before_clusture.png")
-    return g
-
-
-# In[9]:
-
-
-def girvan_newman(G,minsize, maxsize):
-   
-    """
-    Args:
-    G.........a networkx graph
-    minsize...the smallest acceptable size for a community
-    maxsize...the largest acceptable size for a community
-
-    Returns:
-    A list of all discovered communities. This list is a list 
-    of lists of strings. E.g., [['A', 'B'], ['C', 'D']] is a result
-    containing two communities ((A,B) and (C,D)).
-    """
-    if G.order() == 1:
-        return [G.nodes()]
-    
-    def find_best_edge(G0):
-        eb = nx.edge_betweenness_centrality(G0)
-        # eb is dict of (edge, score) pairs, where higher is better
-        # Return the edge with the highest score.
-        return sorted(eb.items(), key=lambda x: x[1], reverse=True)
-
-    # Each component is a separate community. We cluster each of these.
-    
-    components = [c for c in nx.connected_component_subgraphs(G)]
-    edge_to_remove = find_best_edge(G)
-    count = 0
-    while len(components) == 1:
-        G.remove_edge(*edge_to_remove[count][0])
-        count = count +1
-        components = [c for c in nx.connected_component_subgraphs(G)]
-
-    
-    result = []
-    for c in components:
-        if (c.order()>=minsize and c.order()<=maxsize):
-            result.append(c.nodes())
-        elif c.order()>maxsize:
-            result.extend(girvan_newman(c,minsize,maxsize))
-    return result
-
-
-# In[10]:
-
-def draw_network(graph, users, f):
-      
-    ###draw and store the image 
-    name="Cluster"+str(f)+".png"
-    pos=nx.spring_layout(graph)    
-    plt.figure(figsize=(50,50))    
-    nx.draw(graph,pos,with_labels=False,node_size =3000)
-    labels = {}
-   
-    for node in graph.nodes():
-        for i in users:
-            if i == node:
-                labels[node] = i
-    
-    nx.draw_networkx_labels(graph,pos,labels=labels,font_size=55,font_color='b')
-    plt.savefig(name)
-    pass
-
-
-# In[11]:
-
-"""
-store back the result to pickle file
-"""
-def write_file(tweets):
-    fname='Cluster.pkl'
-    print("Cluster Information stored in file",fname)
+#finally write all the collected data back in pickle file
+def write_file(tweets,filename):
+    fname=filename+'.pkl'
+    print("file length after writing->",len(tweets))
     pickle.dump(tweets, open(fname, 'wb'))
+        
 
 
-# In[12]:
+# In[64]:
 
-#main function 
 def main():
+    #establish twitter object
     twitter = get_twitter()
-    tweets=[]
-    names={}   
+    print('Established Twitter connection.')
+    print(' started collecting tweets From Twitter Based on location')
     #team names that are used for analysis
+    #hashtags=['#mufc','#basvafc','#fcbvafc','#arsenal','#championsleague','#ucl','#FCBarcelona','#RealMadrid',
+    #         '#CristianoRonaldo','#cr7','#LeoMessi','#messi','#mancity','#chelsea','#mcfcvcfc','#Celtic']
     hashtags=['#DubNation','#WeTheNorth','#GrindCity','#DetroitBasketball','#thunderup','#LakeShow','#DefendTheLand']
-    count=0
-    """open the file and  find the unique users count
-    """
+   
+    s_id=0
+    #get list of male and females names
+    M,F=names()
     for tags in hashtags:
-        tweets= open_file(tweets,tags)
-        tweeter_names,tweeter_id=find_unique_user(tweets)
-        count+=len(tweeter_names)
-        names[tags]=tweeter_names
-    
-    teams = list(names.keys())
-    #find the mutual users following the each teams
-    for i in range(len(teams)):
-        for j in range(i+1,len(teams)):
-                a =list(set(names[teams[i]] ) & set(names[teams[j]]))
-                print ("Mutual number of followers for the team",teams[i],teams[j],"---->",len(a))           
-    #draw the graph by building the edges betwween the nodes 
-    graph=draw_graph(names) 
-    g=read_graph(graph,names)
-    print('graph has %d nodes and %d edges' %(graph.order(), graph.number_of_edges()))
-    #pass the graph to girvan newman to identify communities
-    result=girvan_newman(g,10, 20)
-    name=1
-    TEMP={}
-    #add the node details and user count details to pickle file for summerize
-    for i in range(1,len(result)):
-        print ('cluster %d  Number of nodes/followers %d' %(i ,len(result[i])))
-        draw_network(graph.subgraph(result[i]),result[i],i)
-        TEMP[i]=result[i]
-        TEMP['usercount']=count
-    write_file(TEMP)
-        
-        
+        tweets = []
+        tweets_from_file =[]
+        since_id=[]
+        #opening file
+        tweets_from_file =open_file(tweets_from_file,tags)
+        # get the minimum Tweet ID to make the collection to be unique
+        if tweets_from_file:    
+            for i in tweets_from_file:
+                since_id.append(i['since_id'])
+            s_id=min(since_id)
+            print (s_id)
+        print("stating")
+        print ("length before search ",len(tweets_from_file))
+        #get the tweets
+        tweets = get_tweets(twitter,tags,tweets,s_id)
+        #filtering the Json response
+        tweets= select_required(tweets,M,F)
+        for i in tweets:
+            tweets_from_file.append(i)
+        #write the contents to file    
+        write_file(tweets_from_file,tags)
+    print('tweets saved to a file called tweets \n')
+
 if __name__ == '__main__':
     main()
+
+
+# In[ ]:
+
+
 
